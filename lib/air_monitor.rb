35cc7fe98f194c9678f7edc9aadf3f0991c8c73e
mckomo <mckomo@gmail.com>
2016-09-27 15:47:01 +0200

    Slight module API change

--- lib/air_monitor.rb
+++ lib/air_monitor.rb
@@ -9,7 +9,7 @@ module AirMonitor
 
   class << self
 
-    def upload_measurements_in(time_range)
+    def upload_measurements_on(time_range)
 
       Station.all.each do |station|
 
@@ -22,7 +22,7 @@ module AirMonitor
         extract_measurements_for(station.channels, response.body)
             .map { |mp| transform(mp) }
             .select { |m| time_range.cover?(m.time) }
-            .lazy
+            .lazy # Load stations lazily: one by one
             .map { |m| load(m) }
             .each { |r| log(r) }
 
@@ -30,9 +30,9 @@ module AirMonitor
 
     end
 
-    def extract_measurements_for(channels, response)
+    def extract_measurements_for(channels, payload)
 
-      measurements_by_subject = response
+      measurements_by_subject = payload
                                     .fetch('data')
                                     .fetch('series')
                                     .map { |s| [s['paramId'], s['data']] }
@@ -42,7 +42,6 @@ module AirMonitor
 
       channels.map { |channel|
 
-        pp
         measurements_by_subject
             .try(:fetch, channel.subject_code.downcase)
             .try(:map) { |m| {value: m[1], time: m[0], channel: channel} }
@@ -61,7 +60,7 @@ module AirMonitor
           value: value,
           time: time,
           channel: channel,
-          source: 'http://monitoring.krakow.pios.gov.pl')
+          source: WIOS::URL)
     end
 
     def load(measurement)
